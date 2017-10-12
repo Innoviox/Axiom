e434f9a76720c18ee9f154ae60a5c484f54cf045
Simon Chervenak <sarras305@gmail.com>
2017-10-12 11:46:50 -0400

    Add files via upload

---
+++ AxiomGL/src/main/java/tutorial/game/Main.java
@@ -0,0 +1,19 @@
+package tutorial.game;
+
+import tutorial.engine.GameEngine;
+import tutorial.engine.IGameLogic;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.start();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
