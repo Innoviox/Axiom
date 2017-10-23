f1680c0e643c6c7490514af700d6af537ec40631
Simon Chervenak <sarras305@gmail.com>
2017-10-23 11:32:19 -0400

    This and last commit fix glitches

--- AxiomGL/resources/shaders/fragment.fs
+++ AxiomGL/resources/shaders/fragment.fs
@@ -4,8 +4,17 @@ in  vec2 outTexCoord;
 out vec4 fragColor;
 
 uniform sampler2D texture_sampler;
+uniform vec3 colour;
+uniform int useColour;
 
 void main()
 {
-    fragColor = texture(texture_sampler, outTexCoord);
+    if ( useColour == 1 )
+    {
+        fragColor = vec4(colour, 1);
+    }
+    else
+    {
+        fragColor = texture(texture_sampler, outTexCoord);
+    }
 }
