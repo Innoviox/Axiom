c9ea1c70b32a81fcacce9878ed2a0db9c00870d5
Innoviox <sarras305@gmail.com>
2017-11-08 14:59:05 -0500

    finished commenting com.axiom.engine.input

--- AxiomGL/src/main/java/com/axiom/engine/input/KeyboardListener.java
+++ AxiomGL/src/main/java/com/axiom/engine/input/KeyboardListener.java
@@ -1,3 +1,15 @@
+/**
+ * Responds to keyboard presses
+ * <p><br>
+ * This class contains methods for interacting with
+ * the keyboard, mouse, and scroll wheel. 
+ * <br>
+ * The mouse of this is deprecated and has been moved
+ * to {@link com.axiom.engine.input.MouseListener}.
+ * </p><p>
+ * @author The Axiom Corp, 2017.
+ * </p>
+ */
 package com.axiom.engine.input;
 
 import org.lwjgl.glfw.*;
@@ -23,16 +35,17 @@ public final class KeyboardListener {
 															// for double click.
 
 	private int NO_STATE = -1;
-
+	// tied to the scroll wheel
 	public GLFWScrollCallback scroll = new GLFWScrollCallback()
     {
-    	public void invoke(long window, double xoffset, double yoffset)
-    	{
-    		scrollStates[0] = xoffset;
-    		scrollStates[1] = yoffset;
-    	}
+	    	public void invoke(long window, double xoffset, double yoffset)
+	    	{
+	    		scrollStates[0] = xoffset;
+	    		scrollStates[1] = yoffset;
+	    	}
     };
 
+    //tied to the keyboard
 	public GLFWKeyCallback keyboard = new GLFWKeyCallback()
     {
         public void invoke(long window, int key, int scancode, int action, int mods)
@@ -40,28 +53,39 @@ public final class KeyboardListener {
             activeKeys[key]=action!=GLFW_RELEASE;keyStates[key]=action;
         }
     };
-    /*
+    
+    @Deprecated
 	public GLFWMouseButtonCallback mouse = new GLFWMouseButtonCallback()
     {
         public void invoke(long window, int button, int action, int mods)
         {
-            activeMouseButtons[button]=action!=GLFW_RELEASE;getMouseButtonStates()[button]=action;}};
-	*/
-	private void resetKeyboard() {
+            activeMouseButtons[button]=action!=GLFW_RELEASE;mouseButtonStates[button]=action;}};
+	
+    /**
+     * Resets the keyboard states
+     */
+	public void resetKeyboard() {
 		for (int i = 0; i < keyStates.length; i++) {
 			keyStates[i] = NO_STATE;
 		}
 	}
 
-	private void resetScroll(){
+	/**
+	 * Resets the scroll states
+	 */
+	public void resetScroll(){
 		for( int i = 0; i < scrollStates.length; i ++ ){
 			scrollStates[i] = 0.0;
 		}
 	}
 	
-	private void resetMouse() {
-		for (int i = 0; i < getMouseButtonStates().length; i++) {
-			getMouseButtonStates()[i] = NO_STATE;
+	@Deprecated
+	/**
+	 * Resets the mouse states
+	 */
+	public void resetMouse() {
+		for (int i = 0; i < mouseButtonStates.length; i++) {
+			mouseButtonStates[i] = NO_STATE;
 		}
 
 		long now = System.nanoTime();
@@ -70,28 +94,64 @@ public final class KeyboardListener {
 			lastMouseNS = 0;
 	}
 
+	/**
+	 * Check if a key is down
+	 * @param key the key to check
+	 * @return key down?
+	 */
 	public boolean keyDown(int key) {
 		return activeKeys[key];
 	}
 
+	/**
+	 * Check if a key is pressed
+	 * @param key the key to check
+	 * @return key pressed?
+	 */
 	public boolean keyPressed(int key) {
 		return keyStates[key] == GLFW_PRESS;
 	}
 
+	/**
+	 * Check if a key is released
+	 * @param key the key to check
+	 * @return key released?
+	 */
 	public boolean keyReleased(int key) {
 		return keyStates[key] == GLFW_RELEASE;
 	}
 
+	@Deprecated
+	/**
+	 * Check if a mouse button is down
+	 * @param button the button to check
+	 * @return button down?
+	 * @deprecated {@link com.axiom.engine.input.MouseListener}
+	 */
 	public boolean mouseButtonDown(int button) {
 		return activeMouseButtons[button];
 	}
 
+	@Deprecated
+	/**
+	 * Check if a mouse button is pressed
+	 * @param button the button to check
+	 * @return button pressed?
+	 * @deprecated {@link com.axiom.engine.input.MouseListener}
+	 */
 	public boolean mouseButtonPressed(int button) {
-		return getMouseButtonStates()[button] == GLFW_RELEASE;
+		return mouseButtonStates[button] == GLFW_RELEASE;
 	}
 
+	@Deprecated
+	/**
+	 * Check if a mouse button is released
+	 * @param button the button to check
+	 * @return button released?
+	 * @deprecated {@link com.axiom.engine.input.MouseListener}
+	 */
 	public boolean mouseButtonReleased(int button) {
-		boolean flag = getMouseButtonStates()[button] == GLFW_RELEASE;
+		boolean flag = mouseButtonStates[button] == GLFW_RELEASE;
 
 		if (flag)
 			lastMouseNS = System.nanoTime();
@@ -99,6 +159,13 @@ public final class KeyboardListener {
 		return flag;
 	}
 
+	@Deprecated
+	/**
+	 * Check if a mouse button is double clicked
+	 * @param button the button to check
+	 * @return button double clicked?
+	 * @deprecated {@link com.axiom.engine.input.MouseListener}
+	 */
 	public boolean mouseButtonDoubleClicked(int button) {
 		long last = lastMouseNS;
 		boolean flag = mouseButtonReleased(button);
@@ -112,15 +179,11 @@ public final class KeyboardListener {
 
 		return false;
 	}
-
-	public int[] getMouseButtonStates() {
-		return mouseButtonStates;
-	}
 	
-	public double[] getScrollStates(){
-		return scrollStates;
-	}
-	
+	/**
+	 * Initialize the input
+	 * @param window the window to tie to
+	 */
     public void init(Window window) {
     		glfwSetKeyCallback(window.getWindowHandle(), this.keyboard);
     		glfwSetScrollCallback(window.getWindowHandle(), this.scroll);
--- AxiomGL/src/main/java/com/axiom/engine/input/MouseListener.java
+++ AxiomGL/src/main/java/com/axiom/engine/input/MouseListener.java
@@ -1,3 +1,14 @@
+/**
+ * Responds to mouse clicking, motion, and position
+ * <p><br>
+ * This class has methods to keep track of the right
+ * and left mouse buttons, cursor motion, the cursor
+ * position, and the display vector to update
+ * the camera based on.
+ * </p><p>
+ * @author Antonio Hernández Bejarano (@lwjglgamedev)
+ * </p>
+ */
 package com.axiom.engine.input;
 
 import org.joml.Vector2d;
@@ -21,12 +32,22 @@ public class MouseListener {
 
     private boolean rightButtonPressed = false;
 
+    /**
+     * Construct a new MouseListner
+     * <br>
+     * Default position: 0, 0
+     * Default display vector: 0, 0
+     */
     public MouseListener() {
         previousPos = new Vector2d(-1, -1);
         currentPos = new Vector2d(0, 0);
         displVec = new Vector2f();
     }
 
+    /**
+     * Initialize the mouseListener's glfw callbacks
+     * @param window the window to tie it to
+     */
     public void init(Window window) {
         glfwSetCursorPosCallback(window.getWindowHandle(), (windowHandle, xpos, ypos) -> {
             currentPos.x = xpos;
@@ -41,10 +62,18 @@ public class MouseListener {
         });
     }
 
+    /**
+     * Get the display vector
+     * @return the display vector
+     */
     public Vector2f getDisplVec() {
         return displVec;
     }
 
+    /**
+     * Update the display vector
+     * @param window the window to tie from
+     */
     public void input(Window window) {
         displVec.x = 0;
         displVec.y = 0;
@@ -64,10 +93,16 @@ public class MouseListener {
         previousPos.y = currentPos.y;
     }
 
+    /**
+     * @return is the left button pressed?
+     */
     public boolean isLeftButtonPressed() {
         return leftButtonPressed;
     }
 
+    /**
+     * @return is the right button pressed?
+     */
     public boolean isRightButtonPressed() {
         return rightButtonPressed;
     }
